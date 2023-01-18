00868cd3bd395b9c5933c9db151dba1223084c49
Guilherme Vieira <vieiragui21@gmail.com>
2023-01-17 22:12:33 -0300

    Colocando texto com o widget Text

--- lib/main.dart
+++ lib/main.dart
@@ -10,39 +10,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center, //alinho o item azul do centro do item vermelho
-            children: [
-              Container(color: Colors.red, width: 100, height: 100,),
-              Container(color: Colors.blue, width: 50, height: 50,),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center, //alinho o item azul do centro do item vermelho
-            children: [
-              Container(color: Colors.blue, width: 100, height: 100,),
-              Container(color: Colors.red, width: 50, height: 50,),
-            ],
-          ),
-          Row(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container(
+          color: Colors.white,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(color: Colors.cyan, width: 50, height: 50,),
-              Container(color: Colors.pinkAccent, width: 50, height: 50,),
-              Container(color: Colors.purple, width: 50, height: 50,),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                //alinho o item azul do centro do item vermelho
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                //alinho o item azul do centro do item vermelho
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.red,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.pinkAccent,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text(
+                  'Testando',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
-          )
-        ],
-      )
-    );
+          ),
+        ));
   }
 }
