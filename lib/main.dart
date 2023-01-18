0bcf370ec94f68994f5b3ea91779d0dc2b049103
Guilherme Vieira <vieiragui21@gmail.com>
2023-01-17 22:06:40 -0300

    Fazendo alinhamento de vários itens na tela

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(color: Colors.red, width: 100, height: 100,),
-          Container(color: Colors.blue, width: 50, height: 50,),
+          Stack(
+            alignment: AlignmentDirectional.center, //alinho o item azul do centro do item vermelho
+            children: [
+              Container(color: Colors.red, width: 100, height: 100,),
+              Container(color: Colors.blue, width: 50, height: 50,),
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center, //alinho o item azul do centro do item vermelho
+            children: [
+              Container(color: Colors.blue, width: 100, height: 100,),
+              Container(color: Colors.red, width: 50, height: 50,),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyan, width: 50, height: 50,),
+              Container(color: Colors.pinkAccent, width: 50, height: 50,),
+              Container(color: Colors.purple, width: 50, height: 50,),
+            ],
+          )
         ],
       )
     );
