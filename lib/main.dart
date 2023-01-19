6a32fce06d4c2ffb13b92a3bde2d9d0d9130d516
Guilherme Vieira <vieiragui21@gmail.com>
2023-01-19 17:46:10 -0300

    Usando Scaffold e inserindo um cabeçalho, corpo e rodapé

--- lib/main.dart
+++ lib/main.dart
@@ -14,85 +14,13 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                //alinho o item azul do centro do item vermelho
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                //alinho o item azul do centro do item vermelho
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.amber,
-                height: 30,
-                width: 300,
-                child: Text(
-                  'Testando',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print('Você apertou o botão');
-                  },
-                  child: Text('Aperte o botão'))
-            ],
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter: Iniciando'),
           ),
-        ));
+          body: Container(),
+          floatingActionButton: FloatingActionButton(onPressed: (){},),
+        ),
+    );
   }
 }
