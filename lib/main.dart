5e089974595f15ad0bccff5dad6dfa7fd5f17c8f
Guilherme Vieira <vieiragui21@gmail.com>
2023-01-20 07:21:30 -0300

    Usando ListView, Padding e Overflow do Text

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
           children: [
             Task("Estudando programação"),
             Task("Aprender a tocar violão"),
@@ -41,31 +40,42 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.black26,
-                  height: 72,
-                  width: 100,
-                ),
-                Text(nome),
-                ElevatedButton(
-                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
             ),
-          ),
-        ],
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    height: 72,
+                    width: 100,
+                  ),
+                  Container(
+                      width: 200,
+                      child: Text(
+                        nome,
+                        style: TextStyle(
+                          fontSize: 24,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
