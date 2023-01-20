c8410a3ba53ac8ecf9f0ec98c47d910e0f8d0741
Guilherme Vieira <vieiragui21@gmail.com>
2023-01-20 07:14:11 -0300

    Criando meu proprio widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,36 +18,54 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(
-                color: Colors.blue,
-                height: 140,
-              ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      height: 72,
-                      width: 100,
-                    ),
-                    Text('Aprender Flutter'),
-                    ElevatedButton(
-                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-                  ],
-                ),
-              ),
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task("Estudando programação"),
+            Task("Aprender a tocar violão"),
+            Task("Aprender nova lingua"),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
         ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+
+  const Task(this.nome, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  height: 72,
+                  width: 100,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
